ca7df61722c240336abcee6af87116afa3d9d55d
Christopher Young <christopheryoung@MacBook123.local>
2024-08-28 18:08:06 -0700

    updated messages-display page

--- src/components/displays/MessagesDisplay.jsx
+++ src/components/displays/MessagesDisplay.jsx
@@ -1,15 +1,23 @@
 import "./MessagesDisplay.scss";
+// React
+import { useEffect, useRef } from "react";
 // Bootstrap
 import ListGroup from 'react-bootstrap/ListGroup';
 
 const MessagesDisplay = ({ messages }) =>{
+  const listRef = useRef(null);
+
+  useEffect(() => {
+    // Scroll to bottom on new messages
+    listRef.current.scrollTop = listRef.current.scrollHeight;
+  }, [messages]);
+
   return (
-    <ListGroup id="messagesDisplay">
+    <ListGroup id="messagesDisplay" ref={listRef}>
       {messages.map((message, idx) => (
         <ListGroup.Item key={idx}>
-          <div>{message.username}</div>
-          <div>{message.text}</div>
-          <div>{message.createdAt}</div>
+          <div className="messagesDisplay-text">{message.username}: {message.text}</div>
+          <div className="messagesDisplay-info">{new Date(message.createdAt).toLocaleString()}</div>
         </ListGroup.Item>
       ))}
     </ListGroup>
